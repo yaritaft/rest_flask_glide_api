397f7c7205dea36fcc358a1ed0f037aefdbc0a59
Yari Taft <yari.taft@intive.com>
2020-06-23 00:31:17 -0300

    Db removed from configs, db removed from travis as well.

---
+++ app/src/config.py
@@ -0,0 +1,28 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig, testing=TestingConfig, prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
